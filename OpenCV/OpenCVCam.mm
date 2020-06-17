2ad52d49a54c254854343284936e2a28848a346b
Vaneesh <vaneesh_k@trigent.com>
2020-06-17 17:06:29 +0530

    Added code for hough circles.

--- OpenCV/OpenCVCam.mm
+++ OpenCV/OpenCVCam.mm
@@ -57,9 +57,46 @@ using namespace cv;
 //    cvtColor(image_copy, bgr, COLOR_GRAY2BGR);
 //    cvtColor(bgr, image, COLOR_BGR2BGRA);
 
+// Do some OpenCV stuff with the image
+    Mat image_copy;
+
+    // Convert to gray scale
+    cvtColor(image, image_copy, COLOR_BGR2GRAY);
+    // invert image
+    bitwise_not(image_copy, image_copy);
+
+    // Lets blur to eliminate noise; feel free to tune the filter size below
+    GaussianBlur(image_copy, image_copy, cv::Size(15, 15), 0);
+
+    // Do a canny. Max threshold value below eliminates most noisy edges and detects the quarter for me; more tuning might be needed
+    Canny(image_copy, image_copy, 100, 150);
+
+    // Now identify the circles; It works best if I have the quarter with just the floor
+    // as the background without my feet.
+    // Like Sae has been advocating, we can achieve this by identifying a box and asking
+    // customers to place the quarter there and not have their feet overlap in that area.
+    // These parameters need further tuning.
+    std::vector<cv::Vec3f> circles;
+    HoughCircles(image_copy, circles, HOUGH_GRADIENT, 1.5, 20, 50, 35, 25, 60);
+
+    cvtColor(image_copy, image_copy, COLOR_GRAY2RGB);
+
+    // I am displaying all the circles found in the code below. But we can do the following
+    // in the actual app:
+    //   1. We can repeat this function until we find exactly one circle
+    //   2. If no circle is found or if we find multiple circles, after a few attempts,
+    // we can ask the user to doublecheck and start again.
+    for( size_t i = 0; i < circles.size(); i++ )
+    {
+        printf("Found %lu circles", circles.size());
+        Vec3i c = circles[i];
+        circle( image_copy, cv::Point(c[0], c[1]), c[2], Scalar(255,0,0), 3, CV_AA);
+        circle( image_copy, cv::Point(c[0], c[1]), 2, Scalar(255,0,0), 3, CV_AA);
+    }
+
     
     if (self.delegate != nil) {
-        [self.delegate imageProcessed:[UIImage imageWithCVMat: image]];
+        [self.delegate imageProcessed:[UIImage imageWithCVMat: image_copy]];
     }
 }
 
--- OpenCV/ViewController.swift
+++ OpenCV/ViewController.swift
@@ -1,32 +1,40 @@
-
 import UIKit
 
-class ViewController: UIViewController, OpenCVCamDelegate {
+class ViewController: UIViewController, OpenCVCamDelegate
+{
 
     @IBOutlet weak var imageView: UIImageView!
-    
+
     var openCVWrapper: OpenCVWrapper!
-    
+
     var lastTimeSet: Double = 0
-    
-    
-    override func viewDidLoad() {
+
+
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         openCVWrapper = OpenCVWrapper()
         openCVWrapper.setDelegate(self)
+        openCVWrapper.start()
     }
 
-    func imageProcessed(_ image: UIImage) {
-        DispatchQueue.main.async {
+    func imageProcessed(_ image: UIImage)
+    {
+        DispatchQueue.main.async
+        {
             self.imageView.image = image
         }
     }
-    
-    @IBAction func start(_ button: UIButton) {
-        openCVWrapper.start()
+
+    @IBAction func start(_ button: UIButton)
+    {
+        let showImage = true
+
+        openCVWrapper.stop()
     }
-    
-    @IBAction func stop(_ button: UIButton) {
+
+    @IBAction func stop(_ button: UIButton)
+    {
         openCVWrapper.stop()
     }
 }
